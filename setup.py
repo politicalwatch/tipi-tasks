8f6bff267f38da93737841006d4281f890248af9
pr3ssh <x@pr3ssh.net>
2020-03-04 11:51:48 +0100

    Change the way to point to tipi-data in setup (v2)

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ setup(
     author='danigm',
     packages=find_packages(),
     install_requires=[
-        'tipi-data @ https://github.com/politicalwatch/tipi-data@master'
-        'git+https://github.com/politicalwatch/tipi-data.git',
+        'tipi-data @ https://github.com/politicalwatch/tipi-data@master',
         'amqp==2.3.2',
         'billiard==3.5.0.5',
         'celery[redis]==4.2.1',
