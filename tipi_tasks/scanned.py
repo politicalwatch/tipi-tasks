cb38b2c87f65aac75c5a020bb2eeeb9e3134bf1d
David Hernandez <davidhernandezruiz@gmail.com>
2020-10-16 15:51:09 +0200

    Fixes task to remove expired scanned docs.

--- tipi_tasks/scanned.py
+++ tipi_tasks/scanned.py
@@ -1,9 +1,10 @@
-from celery import shared_task
+from datetime import datetime, timedelta
+from celery.decorators import periodic_task
 from tipi_data.models.scanned import Scanned
 
 @periodic_task(run_every=timedelta(hours=12))
 def clean_documents():
-    scans = Scanned.objects.filter(expiration__lte=datetime.date.today())
+    scans = Scanned.objects.filter(expiration__lte=datetime.today())
 
     for scan in scans:
         scan.delete()
