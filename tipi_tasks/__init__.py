9359c0cf5dee2933b4cec2aa2fcede5efd18af1d
pr3ssh <x@pr3ssh.net>
2023-04-04 13:07:26 +0200

    Removes Sentry

--- tipi_tasks/__init__.py
+++ tipi_tasks/__init__.py
@@ -1,18 +1,9 @@
 from os import environ as env
 
-import sentry_sdk
 from celery import Celery
-from sentry_sdk.integrations.celery import CeleryIntegration
 
 from . import config
 
-
-SENTRY_DSN = env.get('SENTRY_DSN', None)
-if SENTRY_DSN:
-    sentry_sdk.init(
-        dsn=SENTRY_DSN,
-        integrations=[CeleryIntegration()]
-    )
 
 app = Celery('tasks', broker=config.BROKER, backend=config.RESULT_BACKEND)
 
